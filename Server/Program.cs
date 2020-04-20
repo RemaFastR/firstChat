406687b1a6ca175c559366c602005c05feeb1fb6
Remik Matevosyan <rembimat@gmail.com>
2020-04-20 21:30:13 +0300

    zoro version of my chat

---
+++ Server/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        public static List<Socket> clients = new List<Socket>();
+
+        
+        public static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); 
+        public static short Port = 1234; 
+        static void Main(string[] args)
+        {
+            StartListening();
+            Console.ReadLine();
+        }
+
+        public static void StartListening()
+        {
+            try
+            {
+                Console.WriteLine($"Listening started port:{Port} protocol type: {ProtocolType.Tcp}");
+                socket.Bind(new IPEndPoint(IPAddress.Any, Port));
+                socket.Listen(10);
+                socket.BeginAccept(AcceptCallback, socket);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("listening error" + ex);
+            }
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Console.WriteLine($"Accept CallBack port:{Port} protocol type: {ProtocolType.Tcp}");
+                Socket acceptedSocket = socket.EndAccept(ar);
+                clients.Add(acceptedSocket);
+
+                socket.BeginAccept(AcceptCallback, socket);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Base Accept error" + ex);
+            }
+        }
+        Socket _receiveSocket = socket;
+
+        byte[] _buffer = new byte[1024];
+        public void StartReceiving()
+        {
+           
+
+            try
+            {
+                _buffer = new byte[1024];
+                _receiveSocket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, ReceiveCallback, null);
+            }
+            catch { }
+        }
+
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            try
+            {
+                if (_receiveSocket.EndReceive(AR) > 1)
+                {
+                    // Convert the first 4 bytes (int 32) that we received and convert it to an Int32 (this is the size for the coming data).
+                    _buffer = new byte[BitConverter.ToInt32(_buffer, 0)];
+                    // Next receive this data into the buffer with size that we did receive before
+                    _receiveSocket.Receive(_buffer, _buffer.Length, SocketFlags.None);
+                    string data = Encoding.Default.GetString(_buffer);
+                    Console.WriteLine(data);
+                    // Now we have to start all over again with waiting for a data to come from the socket.
+                    StartReceiving();
+                }
+                else
+                {
+                }
+            }
+            catch
+            {
+               
+            }
+        }
+
+
+
+    }
+}
---
+++ Super_Chat/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Super_Chat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //создем наш сокет
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void connect_button_Click(object sender, RoutedEventArgs e)
+        {
+             await Task.Run(() => socket.Connect("127.0.0.1", 8080));//подключаемся к серверному сокету
+        }
+
+        private async void send_Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(() => sendMessage());
+        }
+
+        void sendMessage()
+        {
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+               (ThreadStart)delegate ()
+               {
+                   string message = messageTB.Text;//получаем текст сообщения
+                   byte[] buffer = Encoding.ASCII.GetBytes(message);//кодируем наше сообщение для отправки
+                   socket.Send(buffer);
+
+                   //ответ сервера
+                   byte[] answer = new byte[1024];
+                   socket.Receive(answer);
+                   chatBoxTB.Text += Encoding.ASCII.GetString(answer);
+
+                   // закрываем сокет
+                  // socket.Shutdown(SocketShutdown.Both);
+                  // socket.Close();
+               }
+            );
+            
+        }
+
+        private void messageTB_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            messageTB.Text = "";
+        }
+    }
+}
